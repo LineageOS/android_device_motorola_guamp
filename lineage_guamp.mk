8610c1bdccdaf40557c82c391c583c16a82bdac7
Michael Bestas <mkbestas@lineageos.org>
2022-10-10 22:37:47 +0300

    guamp: Opt out of EPPE
    Change-Id: I71f54b1ad6de9d1406a3a55239acff1f4e426158

--- lineage_guamp.mk
+++ lineage_guamp.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/guamp/device.mk)
 
 # Inherit some common LineageOS stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
