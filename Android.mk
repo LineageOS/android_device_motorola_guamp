5efd5880e05f6bd3f808747f90bad52b7840c8e0
MD Raza <delightreza@gmail.com>
2023-12-29 19:46:23 +0530

    guamp: Rebrand to guamp
    Change-Id: I4ad1b7fd60bfe3ed9f32308b3e97d8b7a06baea8
    Signed-off-by: MD Raza <delightreza@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), caprip)
+ifeq ($(TARGET_DEVICE), guamp)
 include $(call all-makefiles-under, $(LOCAL_PATH))
 endif
--- device.mk
+++ device.mk
@@ -9,8 +9,7 @@ $(call inherit-product, device/motorola/sm6225-common/bengal.mk)
 
 # A/B
 AB_OTA_PARTITIONS += \
-    vbmeta_system \
-    vendor_boot
+    recovery
 
 # Audio - Configs
 PRODUCT_COPY_FILES += \
@@ -68,13 +67,13 @@ PRODUCT_PACKAGES += \
     init.mmi.overlay.rc
 
 # Shipping API level
-BOARD_API_LEVEL := 30
-BOARD_SHIPPING_API_LEVEL := 30
-PRODUCT_SHIPPING_API_LEVEL := 30
+BOARD_API_LEVEL := 29
+BOARD_SHIPPING_API_LEVEL := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
 
-# Vendor ramdisk
+# Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom_ramdisk:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom_ramdisk:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/motorola/caprip/caprip-vendor.mk)
+$(call inherit-product, vendor/motorola/guamp/guamp-vendor.mk)
